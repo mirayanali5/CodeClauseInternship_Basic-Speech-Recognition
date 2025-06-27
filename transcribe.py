5fb51f6988620250b663bef38f63de51b067e8a2
Mir Ayan Ali <142529805+mirayanali5@users.noreply.github.com>
2025-06-27 17:23:21 +0530

    Delete transcribe.py

--- transcribe.py
+++
@@ -1,9 +0,0 @@
-import whisper
-import ffmpeg
-
-# Load Whisper model (small, medium, or large based on system capacity)
-model = whisper.load_model("base")
-
-def transcribe_audio(file_path):
-    result = model.transcribe(file_path)
-    return result["text"]
